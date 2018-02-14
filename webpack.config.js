2b821303c0cde16992309ca45f2fc2fd5667df10
Ian Busko <ianbusko@Ians-MacBook-Pro.local>
2018-02-14 15:52:55 -0500

    Add basic webpack setup to library

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+   entry: './src/scrollify.js',
+   output: {
+       path: path.resolve(__dirname, 'lib'),
+       filename: 'scrollify .js'
+   },
+   module: {
+       loaders: [
+           {
+               test: /\.js$/,
+               loader: 'babel-loader',
+               query: {
+                   presets: ['env']
+               }
+           }
+       ]
+   },
+   stats: {
+       colors: true
+   },
+   devtool: 'source-map'
+ };
